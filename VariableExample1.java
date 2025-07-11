2660f8447ebf40f394cb6fd1d74adc7a3211eb4f
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-11 22:51:14 +0530

    day4

---
+++ VariableExample1.java
@@ -0,0 +1,11 @@
+public class VariableExample1 {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        String name = "John";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+}
---
+++ VariableExample3.java
@@ -0,0 +1,9 @@
+public class VariableExample3 {
+    public static void main(String[] args) {
+        double radius = 7.0;
+        double pi = 3.14159;
+        double area = pi * radius * radius;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
