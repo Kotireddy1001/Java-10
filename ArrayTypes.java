ef71f4f03f32d42b73985fb4c74bff0aeb415622
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-16 21:47:33 +0530

    datatypes&variables

---
+++ ArrayTypes.java
@@ -0,0 +1,11 @@
+public class ArrayTypes {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30};
+        char[] letters = {'A', 'B', 'C'};
+        String[] names = {"Java", "Python", "C++"};
+
+        System.out.println("First number: " + numbers[0]);
+        System.out.println("First letter: " + letters[0]);
+        System.out.println("First name: " + names[0]);
+    }
+}
---
+++ FinalVariableExample.java
@@ -0,0 +1,9 @@
+public class FinalVariableExample {
+    public static void main(String[] args) {
+        final int MAX = 100;
+        int score = 85;
+
+        System.out.println("Max Score: " + MAX);
+        System.out.println("Your Score: " + score);
+    }
+}
