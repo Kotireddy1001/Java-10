5d41dc7be8b44320e1005d9e714117e69c3112d6
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-18 19:21:11 +0530

    java

---
+++ PrimeCheck.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Check Prime Number using for Loop
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+
+        if (n < 2) isPrime = false;
+        else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(n + (isPrime ? " is Prime" : " is not Prime"));
+    }
+}
