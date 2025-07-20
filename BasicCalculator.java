986e821c2a5a08053f986a464d017519d52f8ac9
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-20 19:40:46 +0530

    java

---
+++ BasicCalculator.java
@@ -0,0 +1,14 @@
+// Arithmetic Calculator using Variables
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+    }
+}
