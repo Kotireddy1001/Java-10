aaea58dc21f1d56cb342dc1853c6bcdbed8e3776
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-20 19:42:37 +0530

    java

---
+++ StudentGrade.java
@@ -0,0 +1,15 @@
+//Student Grade Display
+
+public class StudentGrade {
+    public static void main(String[] args) {
+        String studentName = "Meena";
+        int rollNo = 101;
+        float marks = 87.5f;
+        char grade = 'A';
+
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+    }
+}
