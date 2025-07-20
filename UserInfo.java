78c5a5d020278b56612c45f35e6e5bb191e3d78d
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-20 09:02:10 +0530

    basics

---
+++ UserInfo.java
@@ -0,0 +1,15 @@
+//User Info Display
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "Anjali";
+        int age = 25;
+        double height = 5.4;
+        char gender = 'F';
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " ft");
+        System.out.println("Gender: " + gender);
+    }
+}
