0c1d1eb76a2b17716c5d8c0f608e9eefaf959f08
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-23 22:25:43 +0530

    encapsulation

---
+++ Test5.java
@@ -0,0 +1,21 @@
+//circle area calculation
+
+class Circle {
+    private double radius;
+
+    public void setRadius(double r) {
+        if (r > 0) radius = r;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Test5 {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.setRadius(5);
+        System.out.println("Area: " + c.getArea());
+    }
+}
